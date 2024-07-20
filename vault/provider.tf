4a269bc17ee91c2b6c907005faa700a0db1be1fd
nodadyoushutup <admin@nodadyoushutup.com>
2024-07-20 19:35:50 +0000

    [WIP] Vault

--- vault/provider.tf
+++ vault/provider.tf
@@ -13,6 +13,10 @@ provider "proxmox" {
   api_token = var.api_token
   insecure  = var.insecure
   ssh {
+    node {
+      name = "pve"
+      address = "146.115.179.178"
+    }
     agent = true
     agent_socket = 22
     username = var.username
