8a9b214e3c28be18a6f784ad17de5a2609ade679
nodadyoushutup <admin@nodadyoushutup.com>
2024-07-20 20:48:35 +0000

    [WIP] Vault

--- vault/provider.tf
+++ vault/provider.tf
@@ -11,21 +11,6 @@ terraform {
   }
 }
 
-
-resource "null_resource" "create_temp_file" {
-  provisioner "local-exec" {
-    command = <<EOT
-    echo "${var.private_key}" > "${path.module}/id_rsa.pem"
-    EOT
-  }
-}
-
-# resource "local_file" "ssh_key" {
-#   content  = var.private_key
-#   filename = "${path.module}/id_rsa.pem"
-# }
-
-
 provider "proxmox" {
   endpoint = var.endpoint
   api_token = var.api_token
@@ -38,7 +23,6 @@ provider "proxmox" {
       address = var.address
     }
     username = var.username
-    # password = var.password
     private_key = file("/mnt/workspace/id_rsa")
   }
 }
