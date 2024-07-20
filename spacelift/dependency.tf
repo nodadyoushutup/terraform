20ad5033cddd569b76be2d27ab0ee0f53001057b
nodadyoushutup <admin@nodadyoushutup.com>
2024-07-20 22:33:28 +0000

    [WIP] Spacelift

--- spacelift/dependency.tf
+++ spacelift/dependency.tf
@@ -1,4 +1,4 @@
-resource "spacelift_stack_dependency" "vault_depends_on_proxmox" {
-  stack_id            = spacelift_stack.vault.id
-  depends_on_stack_id = spacelift_stack.proxmox.id
-}
+# resource "spacelift_stack_dependency" "vault_depends_on_proxmox" {
+#   stack_id            = spacelift_stack.vault.id
+#   depends_on_stack_id = spacelift_stack.proxmox.id
+# }
