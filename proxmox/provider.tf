be61cfc85f6e0c76cd477b0c57a159ffce624856
nodadyoushutup <admin@nodadyoushutup.com>
2024-07-20 15:46:59 +0000

    [WIP] Proxmox

---
+++ proxmox/provider.tf
@@ -0,0 +1,14 @@
+terraform {
+  required_providers {
+    proxmox = {
+      source = "bpg/proxmox"
+    }
+  }
+}
+
+provider "proxmox" {
+  endpoint  = var.endpoint
+  username  = var.username
+  password  = var.password
+  insecure  = var.insecure
+}
--- spacelift/stack.tf
+++ spacelift/stack.tf
@@ -1,12 +1,23 @@
 # spacelift/stack.tf
 
-resource "spacelift_stack" "k8s-cluster" {
+resource "spacelift_stack" "proxmox" {
   administrative    = true
   autodeploy        = true
   branch            = "main"
-  description       = "Provisions a Kubernetes cluster"
-  name              = "k3s"
-  project_root      = "k3s"
+  description       = "Provisions Proxmox resources"
+  name              = "proxmox"
+  project_root      = "proxmox"
   repository        = "terraform"
   terraform_version = "1.5.7"
 }
+
+# resource "spacelift_stack" "k8s-cluster" {
+#   administrative    = true
+#   autodeploy        = true
+#   branch            = "main"
+#   description       = "Provisions a Kubernetes cluster"
+#   name              = "k3s"
+#   project_root      = "k3s"
+#   repository        = "terraform"
+#   terraform_version = "1.5.7"
+# }
