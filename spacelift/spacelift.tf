3d5b909d6770a52e2a72e4619f4f87e94595ad8d
nodadyoushutup <admin@nodadyoushutup.com>
2024-07-20 15:19:31 +0000

    [WIP] Spacelift

---
+++ spacelift/spacelift.tf
@@ -0,0 +1,10 @@
+resource "spacelift_stack" "k8s-cluster" {
+  administrative    = true
+  autodeploy        = true
+  branch            = "master"
+  description       = "Provisions a Kubernetes cluster"
+  name              = "Kubernetes Cluster Stack"
+  project_root      = "spacelift"
+  repository        = "terraform"
+  terraform_version = "1.9.2"
+}
