ea388ae70ee34198b1062fef0aa861f1043b57ce
nodadyoushutup <admin@nodadyoushutup.com>
2024-07-21 00:19:56 +0000

    [WIP] Vault

--- vault/main.tf
+++ vault/main.tf
@@ -16,37 +16,37 @@ module "vault" {
   vm_id = 101
   name = "vault"
   tags = ["terraform", "vault"]
-  node_name = "pve"
-  bios = "seabios"
-  on_boot = false
-  started = false
-  stop_on_destroy = true
-  agent_enabled = false
+  # node_name = "pve"
+  # bios = "seabios"
+  # on_boot = false
+  # started = false
+  # stop_on_destroy = true
+  # agent_enabled = false
 
   ipv4_address = "192.168.1.101/24"
-  ipv4_gateway = "192.168.1.1"
+  # ipv4_gateway = "192.168.1.1"
 
   user_account_keys = [
     file("/mnt/workspace/desktop.pub"),
     file("/mnt/workspace/proxmox.pub"),
   ]
   user_account_password = var.password
-  user_account_username = "ubuntu"
+  # user_account_username = "ubuntu"
   
-  disk_datastore_id = "local-lvm"
-  disk_file_id = "local:iso/jammy-server-cloudimg-amd64.img"
-  disk_interface = "scsi0"
-  disk_discard = "on"
-  disk_size = 50
-  disk_ssd = true
+  # disk_datastore_id = "local-lvm"
+  # disk_file_id = "local:iso/jammy-server-cloudimg-amd64.img"
+  # disk_interface = "scsi0"
+  # disk_discard = "on"
+  # disk_size = 50
+  # disk_ssd = true
   
-  network_bridge = "vmbr0"
-  network_model = "e1000e"
+  # network_bridge = "vmbr0"
+  # network_model = "e1000e"
   
-  memory_dedicated = 4096
+  # memory_dedicated = 4096
   
-  cpu_cores = 2
-  cpu_sockets = 1
-  cpu_type = "host"
-  cpu_numa = true
+  # cpu_cores = 2
+  # cpu_sockets = 1
+  # cpu_type = "host"
+  # cpu_numa = true
 }
