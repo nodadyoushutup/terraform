57faf0077de28c349b9729d7d5eb71147b86a683
nodadyoushutup <admin@nodadyoushutup.com>
2024-07-21 22:02:29 +0000

    [WIP] Vault

--- vault/variables.tf
+++ vault/variables.tf
@@ -1,33 +1,33 @@
-# Provider Config
-variable "endpoint" {
-  description = "Proxmox API endpoint"
-  type        = string
-}
+# # Provider Config
+# variable "endpoint" {
+#   description = "Proxmox API endpoint"
+#   type        = string
+# }
 
-variable "username" {
-  description = "Proxmox API username"
-  type        = string
-}
+# variable "username" {
+#   description = "Proxmox API username"
+#   type        = string
+# }
 
-variable "password" {
-  description = "Proxmox API password"
-  type        = string
-  sensitive   = true
-  default     = ""
-}
+# variable "password" {
+#   description = "Proxmox API password"
+#   type        = string
+#   sensitive   = true
+#   default     = ""
+# }
 
-variable "insecure" {
-  description = "Allow insecure API connections"
-  type        = bool
-  default     = false
-}
+# variable "insecure" {
+#   description = "Allow insecure API connections"
+#   type        = bool
+#   default     = false
+# }
 
-variable "api_token" {
-  description = "Proxmox API token"
-  type        = string
-}
+# variable "api_token" {
+#   description = "Proxmox API token"
+#   type        = string
+# }
 
-variable "address" {
-  description = "PVE Node IP Address"
-  type        = string
-}
+# variable "address" {
+#   description = "PVE Node IP Address"
+#   type        = string
+# }
