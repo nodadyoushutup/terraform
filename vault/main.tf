949aa2c7173e729e375eb70c6ab83e532e8a832f
nodadyoushutup <admin@nodadyoushutup.com>
2024-07-21 22:43:25 +0000

    [WIP] Vault

--- vault/main.tf
+++ vault/main.tf
@@ -27,40 +27,10 @@ module "virtual_machine" {
   source  = "spacelift.io/nodadyoushutup/virtual_machine/proxmox"
   version = "0.1.8"
 
-  # Required inputs 
   address      = var.address
   api_token    = var.api_token
   endpoint     = var.endpoint
-  ipv4_address = "192.168.1.101"
+  ipv4_address = "192.168.1.101/24"
   username     = var.username
   vm_id        = "101"
-
-  # Optional inputs 
-  # agent_enabled         = bool
-  # bios                  = string
-  # cpu_cores             = number
-  # cpu_numa              = bool
-  # cpu_sockets           = number
-  # cpu_type              = string
-  # disk_datastore_id     = string
-  # disk_discard          = string
-  # disk_file_id          = string
-  # disk_interface        = string
-  # disk_size             = number
-  # disk_ssd              = bool
-  # insecure              = bool
-  # ipv4_gateway          = string
-  # memory_dedicated      = number
-  # name                  = string
-  # network_bridge        = string
-  # network_model         = string
-  # node_name             = string
-  # on_boot               = bool
-  # password              = string
-  # started               = bool
-  # stop_on_destroy       = bool
-  # tags                  = list(string)
-  # user_account_keys     = list(string)
-  # user_account_password = string
-  # user_account_username = string
 }
---
+++ vault/variable.tf
@@ -0,0 +1,195 @@
+
+variable "endpoint" {
+  description = "Proxmox API endpoint"
+  type        = string
+}
+
+variable "username" {
+  description = "Proxmox API username"
+  type        = string
+}
+
+variable "password" {
+  description = "Proxmox API password"
+  type        = string
+  sensitive   = true
+  default     = ""
+}
+
+variable "insecure" {
+  description = "Allow insecure API connections"
+  type        = bool
+  default     = false
+}
+
+variable "api_token" {
+  description = "Proxmox API token"
+  type        = string
+}
+
+variable "address" {
+  description = "PVE Node IP Address"
+  type        = string
+}
+
+# VM Config
+
+variable "name" {
+  description = "Virtual machine name"
+  type        = string
+  default     = "ubuntu"
+}
+
+variable "tags" {
+  description = "An example list variable"
+  type        = list(string)
+  default     = ["terraform"]
+}
+
+variable "node_name" {
+  description = "Node name"
+  type        = string
+  default     = "pve"
+}
+
+variable "bios" {
+  description = "BIOS type"
+  type        = string
+  default     = "seabios"
+}
+
+variable "on_boot" {
+  description = "Start VM on boot"
+  type        = bool
+  default     = false
+}
+
+variable "started" {
+  description = "Start VM immediately after creation"
+  type        = bool
+  default     = false
+}
+
+variable "stop_on_destroy" {
+  description = "Stop VM on destroy"
+  type        = bool
+  default     = true
+}
+
+variable "agent_enabled" {
+  description = "Enable agent"
+  type        = bool
+  default     = false
+}
+
+variable "ipv4_address" {
+  description = "IPv4 address"
+  type        = string
+}
+
+variable "ipv4_gateway" {
+  description = "IPv4 gateway"
+  type        = string
+  default     = "192.168.1.1"
+}
+
+variable "user_account_keys" {
+  description = "User account keys"
+  type        = list(string)
+  default     = []
+}
+
+variable "user_account_password" {
+  description = "User account password"
+  type        = string
+  default     = "ubuntu"
+}
+
+variable "user_account_username" {
+  description = "User account username"
+  type        = string
+  default     = "ubuntu"
+}
+
+variable "disk_datastore_id" {
+  description = "Disk datastore ID"
+  type        = string
+  default     = "local-lvm"
+}
+
+variable "vm_id" {
+  description = "VM ID"
+  type        = string
+}
+
+variable "disk_size" {
+  description = "Disk size in GB"
+  type        = number
+  default     = 50
+}
+
+variable "disk_file_id" {
+  description = "Disk file ID"
+  type        = string
+  default     = "local:iso/jammy-server-cloudimg-amd64.img"
+}
+
+variable "disk_interface" {
+  description = "Disk interface type"
+  type        = string
+  default     = "scsi0"
+}
+
+variable "disk_discard" {
+  description = "Disk discard option"
+  type        = string
+  default     = "on"
+}
+
+variable "disk_ssd" {
+  description = "Is the disk SSD?"
+  type        = bool
+  default     = true
+}
+
+variable "network_bridge" {
+  description = "Network bridge"
+  type        = string
+  default     = "vmbr0"
+}
+
+variable "network_model" {
+  description = "Network model"
+  type        = string
+  default     = "e1000e"
+}
+
+variable "memory_dedicated" {
+  description = "Dedicated memory in MB"
+  type        = number
+  default     = 4096
+}
+
+variable "cpu_cores" {
+  description = "Number of CPU cores"
+  type        = number
+  default     = 2
+}
+
+variable "cpu_sockets" {
+  description = "Number of CPU sockets"
+  type        = number
+  default     = 1
+}
+
+variable "cpu_type" {
+  description = "CPU type"
+  type        = string
+  default     = "host"
+}
+
+variable "cpu_numa" {
+  description = "Enable NUMA"
+  type        = bool
+  default     = true
+}
