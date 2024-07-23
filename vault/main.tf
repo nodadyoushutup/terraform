d632fa4515bb43ad50846e50156b34c108493ce1
nodadyoushutup <admin@nodadyoushutup.com>
2024-07-22 22:26:47 -0400

    [WIP] Vault

--- vault/main.tf
+++ vault/main.tf
@@ -8,12 +8,13 @@ module "vault" {
   source  = "spacelift.io/nodadyoushutup/virtual_machine/proxmox"
   version = "0.1.9"
 
-  address      = local.config.provider.address
-  api_token    = local.config.provider.api_token
-  endpoint     = local.config.provider.endpoint
-  username     = local.config.provider.username
-  ipv4_address = "192.168.1.101/24"
-  vm_id        = "101"
+  address       = local.config.provider.address
+  api_token     = local.config.provider.api_token
+  endpoint      = local.config.provider.endpoint
+  username      = local.config.provider.username
+  insecure      = true
+  ipv4_address  = "192.168.1.101/24"
+  vm_id         = "101"
 }
 
 output "provider_endpoint_value" {
