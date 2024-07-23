bbb1ea531395d22294d6c11a135b74ff26021e7d
nodadyoushutup <admin@nodadyoushutup.com>
2024-07-22 22:19:38 -0400

    [WIP] Vault

--- vault/main.tf
+++ vault/main.tf
@@ -1,28 +1,5 @@
 # vault/main.tf
 
-# module "vault" {
-#   source  = "spacelift.io/nodadyoushutup/virtual_machine/proxmox"
-#   version = "0.1.8"
-
-#   # Provider Config
-#   username = var.username
-#   endpoint = var.endpoint
-#   address = var.address
-#   insecure = var.insecure
-#   api_token = var.api_token
-
-#   # VM Config
-#   vm_id = 101
-#   name = "vault"
-#   tags = ["terraform", "init", "vault"]
-#   ipv4_address = "192.168.1.101/24"
-#   user_account_keys = [
-#     file("/mnt/workspace/desktop.pub"),
-#     file("/mnt/workspace/proxmox.pub"),
-#   ]
-#   user_account_password = var.user_account_password
-# }
-
 locals {
   config = yamldecode(file("/mnt/workspace/provider.yaml"))
 }
