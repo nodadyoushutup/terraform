1d2c35db70ddcf73362ae2db81758524ab70947b
nodadyoushutup <admin@nodadyoushutup.com>
2024-07-22 22:03:49 -0400

    [WIP] Vault

--- vault/main.tf
+++ vault/main.tf
@@ -23,21 +23,24 @@
 #   user_account_password = var.user_account_password
 # }
 
-module "virtual_machine" {
-  source  = "spacelift.io/nodadyoushutup/virtual_machine/proxmox"
-  version = "0.1.9"
-
-  address      = "ssh.proxmox.nodadyoushutup.com"
-  api_token    = "root@pve!provider=85e05ca2-aa11-4813-af6d-35a4d1f188b8"
-  endpoint     = "https://proxmox.nodadyoushutup.com/"
-  ipv4_address = "192.168.1.101/24"
-  username     = "root"
-  vm_id        = "101"
+locals {
+  config = yamldecode(file("/mnt/workspace/provider.yaml"))
 }
 
+# module "virtual_machine" {
+#   source  = "spacelift.io/nodadyoushutup/virtual_machine/proxmox"
+#   version = "0.1.9"
+
+#   address      = 
+#   api_token    = "root@pve!provider=85e05ca2-aa11-4813-af6d-35a4d1f188b8"
+#   endpoint     = "https://proxmox.nodadyoushutup.com/"
+#   ipv4_address = "192.168.1.101/24"
+#   username     = "root"
+#   vm_id        = "101"
+# }
+
 output "provider_endpoint_value" {
-  value = getenv("PROVIDER_ENDPOINT")
-  # value = lookup("PROVIDER_ENDPOINT")
-  description = "The value of the PROVIDER_ENDPOINT environment variable"
+  value = local.config
+  description = "Test"
 }
 
