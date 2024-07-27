4437115126cacbee9a28b416b86d6cbf59988593
nodadyoushutup <admin@nodadyoushutup.com>
2024-07-27 17:43:38 +0000

    [WIP] Vault

--- vault/main.tf
+++ vault/main.tf
@@ -8,9 +8,9 @@ terraform {
   }
 }
 
-module "vault" {
-  source  = "spacelift.io/nodadyoushutup/virtual_machine/proxmox"
-  version = "0.1.12"
-  ipv4_address  = "192.168.1.101/24"
-  vm_id         = "101"
-}
+# module "vault" {
+#   source  = "spacelift.io/nodadyoushutup/virtual_machine/proxmox"
+#   version = "0.1.12"
+#   ipv4_address  = "192.168.1.101/24"
+#   vm_id         = "101"
+# }
