b3c5a12ea9a199ab17e45d7ec9e01b3a016d1fee
nodadyoushutup <admin@nodadyoushutup.com>
2024-07-27 17:42:20 +0000

    [WIP] Proxmox

--- vault/main.tf
+++ vault/main.tf
@@ -1,5 +1,13 @@
 # vault/main.tf
 
+terraform {
+  required_providers {
+    proxmox = {
+      source = "bpg/proxmox"
+    }
+  }
+}
+
 # module "vault" {
 #   source  = "spacelift.io/nodadyoushutup/virtual_machine/proxmox"
 #   version = "0.1.12"
