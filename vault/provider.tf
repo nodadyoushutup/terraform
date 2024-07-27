627e9bb37ffd5e6eed983e019749bdff4c160202
nodadyoushutup <admin@nodadyoushutup.com>
2024-07-27 17:58:14 +0000

    [WIP] Vault

--- vault/provider.tf
+++ vault/provider.tf
@@ -1,10 +1,10 @@
 
 # vault/provider.tf
 
-terraform {
-  required_providers {
-    proxmox = {
-      source = "bpg/proxmox"
-    }
-  }
-}
+# terraform {
+#   required_providers {
+#     proxmox = {
+#       source = "bpg/proxmox"
+#     }
+#   }
+# }
