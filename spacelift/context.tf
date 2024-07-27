59ff0e6b911c52730cf8e4fa49c014679dfd641b
nodadyoushutup <admin@nodadyoushutup.com>
2024-07-27 16:33:28 +0000

    [WIP] Proxmox

--- spacelift/context.tf
+++ spacelift/context.tf
@@ -2,3 +2,8 @@ resource "spacelift_context" "debug" {
   description = "DEBUG level logs"
   name        = "debug"
 }
+
+resource "spacelift_context" "provider_proxmox_ip" {
+  description = "Proxmox provider credentials"
+  name        = "provider-proxmox-ip"
+}
--- spacelift/context_attachment.tf
+++ spacelift/context_attachment.tf
@@ -3,3 +3,9 @@ resource "spacelift_context_attachment" "debug" {
   stack_id   = "vault"
   priority   = 0
 }
+
+resource "spacelift_context_attachment" "provider_proxmox_ip" {
+  context_id = "provider_proxmox_ip"
+  stack_id   = "proxmox"
+  priority   = 0
+}
