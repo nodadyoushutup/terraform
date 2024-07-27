5d7efa69cbfd60767b84cad03a38d2cf1f2451c2
nodadyoushutup <admin@nodadyoushutup.com>
2024-07-27 17:45:13 +0000

    [WIP] Proxmox

--- proxmox/image.tf
+++ proxmox/image.tf
@@ -1,33 +1,33 @@
 # proxmox/image/tf
 
-resource "proxmox_virtual_environment_download_file" "ubuntu_bionic_18_04_cloud_image" {
-  content_type = "iso"
-  datastore_id = "local"
-  node_name    = "pve"
-  url = "https://cloud-images.ubuntu.com/bionic/current/bionic-server-cloudimg-amd64.img"
-  overwrite_unmanaged = true
-}
+# resource "proxmox_virtual_environment_download_file" "ubuntu_bionic_18_04_cloud_image" {
+#   content_type = "iso"
+#   datastore_id = "local"
+#   node_name    = "pve"
+#   url = "https://cloud-images.ubuntu.com/bionic/current/bionic-server-cloudimg-amd64.img"
+#   overwrite_unmanaged = true
+# }
 
-resource "proxmox_virtual_environment_download_file" "ubuntu_focal_20_04_cloud_image" {
-  content_type = "iso"
-  datastore_id = "local"
-  node_name    = "pve"
-  url = "https://cloud-images.ubuntu.com/focal/current/focal-server-cloudimg-amd64.img"
-  overwrite_unmanaged = true
-}
+# resource "proxmox_virtual_environment_download_file" "ubuntu_focal_20_04_cloud_image" {
+#   content_type = "iso"
+#   datastore_id = "local"
+#   node_name    = "pve"
+#   url = "https://cloud-images.ubuntu.com/focal/current/focal-server-cloudimg-amd64.img"
+#   overwrite_unmanaged = true
+# }
 
-resource "proxmox_virtual_environment_download_file" "ubuntu_jammy_22_04_cloud_image" {
-  content_type = "iso"
-  datastore_id = "local"
-  node_name    = "pve"
-  url = "https://cloud-images.ubuntu.com/jammy/current/jammy-server-cloudimg-amd64.img"
-  overwrite_unmanaged = true
-}
+# resource "proxmox_virtual_environment_download_file" "ubuntu_jammy_22_04_cloud_image" {
+#   content_type = "iso"
+#   datastore_id = "local"
+#   node_name    = "pve"
+#   url = "https://cloud-images.ubuntu.com/jammy/current/jammy-server-cloudimg-amd64.img"
+#   overwrite_unmanaged = true
+# }
 
-resource "proxmox_virtual_environment_download_file" "ubuntu_noble_22_04_cloud_image" {
-  content_type = "iso"
-  datastore_id = "local"
-  node_name    = "pve"
-  url = "https://cloud-images.ubuntu.com/noble/current/noble-server-cloudimg-amd64.img"
-  overwrite_unmanaged = true
-}
+# resource "proxmox_virtual_environment_download_file" "ubuntu_noble_22_04_cloud_image" {
+#   content_type = "iso"
+#   datastore_id = "local"
+#   node_name    = "pve"
+#   url = "https://cloud-images.ubuntu.com/noble/current/noble-server-cloudimg-amd64.img"
+#   overwrite_unmanaged = true
+# }
--- proxmox/provider.tf
+++ proxmox/provider.tf
@@ -4,26 +4,33 @@ locals {
   provider = yamldecode(file("/mnt/workspace/provider.yaml"))
 }
 
-terraform {
-  required_providers {
-    proxmox = {
-      source = "bpg/proxmox"
-    }
-  }
+output "provider_endpoint_value" {
+  value = local.provider
+  description = "Provider credentials"
 }
 
-provider "proxmox" {
-  endpoint = local.provider.proxmox.endpoint
-  api_token = local.provider.proxmox.api_token
-  insecure  = local.provider.proxmox.insecure
-  ssh {
-    agent = true
-    agent_socket = 22
-    username = local.provider.proxmox.username
-    private_key = file("/mnt/workspace/proxmox.pem")
-    node {
-      name = local.provider.proxmox.node_name
-      address = local.provider.proxmox.node_address
-    }
-  }
-}
+
+
+# terraform {
+#   required_providers {
+#     proxmox = {
+#       source = "bpg/proxmox"
+#     }
+#   }
+# }
+
+# provider "proxmox" {
+#   endpoint = local.provider.proxmox.endpoint
+#   api_token = local.provider.proxmox.api_token
+#   insecure  = local.provider.proxmox.insecure
+#   ssh {
+#     agent = true
+#     agent_socket = 22
+#     username = local.provider.proxmox.username
+#     private_key = file("/mnt/workspace/proxmox.pem")
+#     node {
+#       name = local.provider.proxmox.node_name
+#       address = local.provider.proxmox.node_address
+#     }
+#   }
+# }
