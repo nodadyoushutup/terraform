398adffe2aef0ba60594d5d4de24a1150c105993
nodadyoushutup <admin@nodadyoushutup.com>
2024-07-27 17:49:36 +0000

    [WIP] Proxmox

--- proxmox/provider.tf
+++ proxmox/provider.tf
@@ -4,33 +4,33 @@ locals {
   provider = yamldecode(file("/mnt/workspace/provider.yaml"))
 }
 
-output "provider_endpoint_value" {
+output "provider" {
   value = local.provider
   description = "Provider credentials"
 }
 
 
 
-# terraform {
-#   required_providers {
-#     proxmox = {
-#       source = "bpg/proxmox"
-#     }
-#   }
-# }
+terraform {
+  required_providers {
+    proxmox = {
+      source = "bpg/proxmox"
+    }
+  }
+}
 
-# provider "proxmox" {
-#   endpoint = local.provider.proxmox.endpoint
-#   api_token = local.provider.proxmox.api_token
-#   insecure  = local.provider.proxmox.insecure
-#   ssh {
-#     agent = true
-#     agent_socket = 22
-#     username = local.provider.proxmox.username
-#     private_key = file("/mnt/workspace/proxmox.pem")
-#     node {
-#       name = local.provider.proxmox.node_name
-#       address = local.provider.proxmox.node_address
-#     }
-#   }
-# }
+provider "proxmox" {
+  endpoint = local.provider.proxmox.endpoint
+  api_token = local.provider.proxmox.api_token
+  insecure  = local.provider.proxmox.insecure
+  ssh {
+    agent = true
+    agent_socket = 22
+    username = local.provider.proxmox.username
+    private_key = file("/mnt/workspace/proxmox.pem")
+    node {
+      name = local.provider.proxmox.node_name
+      address = local.provider.proxmox.node_address
+    }
+  }
+}
