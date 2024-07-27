cb6b6e5efa5696829d64c2aac21416e5ebbd842f
nodadyoushutup <admin@nodadyoushutup.com>
2024-07-27 17:58:48 +0000

    [WIP] Vault

--- vault/main.tf
+++ vault/main.tf
@@ -4,5 +4,5 @@ module "vault" {
   source  = "spacelift.io/nodadyoushutup/virtual_machine/proxmox"
   version = "0.1.12"
   ipv4_address  = "192.168.1.101/24"
-  vm_id         = "101"
+  vm_id         = "102"
 }
