8cbce6e00279c261b3395188192f99a109ac78df
nodadyoushutup <admin@nodadyoushutup.com>
2024-07-27 17:29:26 +0000

    [WIP] Vault

--- vault/main.tf
+++ vault/main.tf
@@ -4,15 +4,15 @@
 #   config = yamldecode(file("/mnt/workspace/provider.yaml"))
 # }
 
-# module "vault" {
-#   source  = "spacelift.io/nodadyoushutup/virtual_machine/proxmox"
-#   version = "0.1.9"
+module "vault" {
+  source  = "spacelift.io/nodadyoushutup/virtual_machine/proxmox"
+  version = "0.1.11"
 
 #   address       = local.config.address
 #   api_token     = local.config.api_token
 #   endpoint      = local.config.endpoint
 #   username      = local.config.username
 #   insecure      = local.config.insecure
-#   ipv4_address  = "192.168.1.101/24"
-#   vm_id         = "101"
-# }
+  ipv4_address  = "192.168.1.101/24"
+  vm_id         = "101"
+}
