caaf5ab71079b1310815d9e167b75f6971b354f2
nodadyoushutup <admin@nodadyoushutup.com>
2024-07-27 17:07:23 +0000

    [WIP] Proxmox

--- proxmox/provider.tf
+++ proxmox/provider.tf
@@ -1,25 +1,30 @@
 # proxmox/provider./tf
 
-terraform {
-  required_providers {
-    proxmox = {
-      source = "bpg/proxmox"
-    }
-  }
-}
+# terraform {
+#   required_providers {
+#     proxmox = {
+#       source = "bpg/proxmox"
+#     }
+#   }
+# }
 
-provider "proxmox" {
-  endpoint = var.endpoint
-  api_token = var.api_token
-  insecure  = var.insecure
-  ssh {
-    agent = true
-    agent_socket = 22
-    username = var.username
-    private_key = file("/mnt/workspace/proxmox.pem")
-    node {
-      name = "pve"
-      address = var.address
-    }
-  }
+# provider "proxmox" {
+#   endpoint = var.endpoint
+#   api_token = var.api_token
+#   insecure  = var.insecure
+#   ssh {
+#     agent = true
+#     agent_socket = 22
+#     username = var.username
+#     private_key = file("/mnt/workspace/proxmox.pem")
+#     node {
+#       name = "pve"
+#       address = var.address
+#     }
+#   }
+# }
+
+module "proxmox_provider" {
+  source = "spacelift.io/nodadyoushutup/provider_proxmox/proxmox"
+  version = "0.0.2"
 }
--- vault/output.tf
+++ vault/output.tf
@@ -1,5 +1,5 @@
-output "provider_endpoint_value" {
-  value = local.config
-  description = "Test"
-}
+# output "provider_endpoint_value" {
+#   value = local.config
+#   description = "Test"
+# }
 
