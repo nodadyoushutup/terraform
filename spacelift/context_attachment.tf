7a6b7c2910db03fe5c39d8066e98d6eb4464984d
nodadyoushutup <admin@nodadyoushutup.com>
2024-07-27 16:49:15 +0000

    [WIP] Spacelift

--- spacelift/context_attachment.tf
+++ spacelift/context_attachment.tf
@@ -1,11 +1,30 @@
-resource "spacelift_context_attachment" "debug" {
+resource "spacelift_context_attachment" "debug_vault" {
   context_id = "debug"
   stack_id   = "vault"
   priority   = 0
 }
 
-resource "spacelift_context_attachment" "provider_proxmox_ip" {
+resource "spacelift_context_attachment" "debug_spacelift" {
+  context_id = "debug"
+  stack_id   = "spacelift"
+  priority   = 0
+}
+
+resource "spacelift_context_attachment" "debug_proxmox" {
+  context_id = "debug"
+  stack_id   = "proxmox"
+  priority   = 0
+}
+
+
+resource "spacelift_context_attachment" "provider_proxmox_ip_proxmox" {
   context_id = "provider_proxmox_ip"
   stack_id   = "proxmox"
+  priority   = 0
+}
+
+resource "spacelift_context_attachment" "provider_proxmox_ip_vault" {
+  context_id = "provider_proxmox_ip"
+  stack_id   = "vault"
   priority   = 0
 }
