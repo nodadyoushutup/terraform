29c24522959826a42a17ef81f959b992e22bfd2d
nodadyoushutup <admin@nodadyoushutup.com>
2024-07-27 17:11:34 +0000

    [WIP] Proxmox

--- proxmox/provider.tf
+++ proxmox/provider.tf
@@ -1,12 +1,12 @@
 # proxmox/provider./tf
 
-# terraform {
-#   required_providers {
-#     proxmox = {
-#       source = "bpg/proxmox"
-#     }
-#   }
-# }
+terraform {
+  required_providers {
+    proxmox = {
+      source = "bpg/proxmox"
+    }
+  }
+}
 
 # provider "proxmox" {
 #   endpoint = var.endpoint
