64605452a9fd58c0c45ace4c53ed21bdb8d90a1e
nodadyoushutup <admin@nodadyoushutup.com>
2024-07-28 20:42:39 +0000

    [WIP] Virtual Machine

--- vault/main.tf
+++ vault/main.tf
@@ -45,12 +45,12 @@ module "vault" {
   
 
   initialization = {
-    ip_config = {
+    # ip_config = {
     #   ipv4 = {
     #     address = "192.168.1.101/24"
     #     gateway = "192.168.1.1"
     #   }
-    }
+    # }
     # user_account = {
     #   username = local.virtual_machine.username
     #   password = local.virtual_machine.password
