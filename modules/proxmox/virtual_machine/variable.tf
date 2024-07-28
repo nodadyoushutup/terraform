412d1e35602910816a530aaf251b48e861c7219d
nodadyoushutup <admin@nodadyoushutup.com>
2024-07-28 18:25:09 +0000

    [WIP] Virtual Machine

--- modules/proxmox/virtual_machine/variable.tf
+++ modules/proxmox/virtual_machine/variable.tf
@@ -57,7 +57,7 @@ variable "bios" {
   type = string
   default = "seabios"
   validation {
-    condition = contains(["ovmf", "seabios"], var.audio_device.driver)
+    condition = contains(["ovmf", "seabios"], var.bios)
     error_message = "Inalid instance type. Valid options are ['ovmf', 'seabios']"
   }
 }
