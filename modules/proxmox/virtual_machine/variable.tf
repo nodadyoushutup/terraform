f9f8248c844a0e42844ecfb35d4f5a09d3b331e5
nodadyoushutup <admin@nodadyoushutup.com>
2024-07-29 01:58:15 +0000

    [WIP] Virtual Machine

--- modules/proxmox/virtual_machine/variable.tf
+++ modules/proxmox/virtual_machine/variable.tf
@@ -150,47 +150,44 @@ variable "cpu" {
     ])
   }
   validation {
-    condition = var.cpu == null || alltrue([
-      for flag in var.cpu.type :
-        can(
-          regex(
-            join("", [
-              "^(",
-              "486|",
-              "Broadwell|Broadwell-IBRS|Broadwell-noTSX|Broadwell-noTSX-IBRS|",
-              "Cascadelake-Server|Cascadelake-Server-noTSX|Cascadelake-Server-v2|Cascadelake-Server-v4|Cascadelake-Server-v5|",
-              "Conroe|",
-              "Cooperlake|Cooperlake-v2|",
-              "EPYC|EPYC-IBPB|EPYC-Milan|EPYC-Rome|EPYC-Rome-v2|EPYC-v3|",
-              "Haswell|Haswell-IBRS|Haswell-noTSX|Haswell-noTSX-IBRS|",
-              "Icelake-Client|Icelake-Client-noTSX|",
-              "Icelake-Server|Icelake-Server-noTSX|Icelake-Server-v3|Icelake-Server-v4|Icelake-Server-v5|Icelake-Server-v6|",
-              "IvyBridge|IvyBridge-IBRS|",
-              "KnightsMill|",
-              "Nehalem|Nehalem-IBRS|",
-              "Opteron_G1|Opteron_G2|Opteron_G3|Opteron_G4|Opteron_G5|",
-              "Penryn|",
-              "SandyBridge|SandyBridge-IBRS|",
-              "SapphireRapids|",
-              "Skylake-Client|Skylake-Client-IBRS|Skylake-Client-noTSX-IBRS|Skylake-Client-v4|",
-              "Skylake-Server|Skylake-Server-IBRS|Skylake-Server-noTSX-IBRS|Skylake-Server-v4|Skylake-Server-v5|",
-              "Westmere|Westmere-IBRS|",
-              "athlon|",
-              "core2duo|coreduo|",
-              "host|",
-              "kvm32|kvm64|",
-              "max|",
-              "pentium|pentium2|pentium3|",
-              "phenom|",
-              "qemu32|qemu64|",
-              "x86-64-v2|x86-64-v2-AES|x86-64-v3|x86-64-v4|",
-              "custom-[A-Za-z0-9@#$%^&*()-_=+]+",
-              ")$"
-            ]), 
-            flag
-          )
-        )
-    ])
+    condition = var.cpu == null || can(
+      regex(
+        join("", [
+          "^(",
+          "486|",
+          "Broadwell|Broadwell-IBRS|Broadwell-noTSX|Broadwell-noTSX-IBRS|",
+          "Cascadelake-Server|Cascadelake-Server-noTSX|Cascadelake-Server-v2|Cascadelake-Server-v4|Cascadelake-Server-v5|",
+          "Conroe|",
+          "Cooperlake|Cooperlake-v2|",
+          "EPYC|EPYC-IBPB|EPYC-Milan|EPYC-Rome|EPYC-Rome-v2|EPYC-v3|",
+          "Haswell|Haswell-IBRS|Haswell-noTSX|Haswell-noTSX-IBRS|",
+          "Icelake-Client|Icelake-Client-noTSX|",
+          "Icelake-Server|Icelake-Server-noTSX|Icelake-Server-v3|Icelake-Server-v4|Icelake-Server-v5|Icelake-Server-v6|",
+          "IvyBridge|IvyBridge-IBRS|",
+          "KnightsMill|",
+          "Nehalem|Nehalem-IBRS|",
+          "Opteron_G1|Opteron_G2|Opteron_G3|Opteron_G4|Opteron_G5|",
+          "Penryn|",
+          "SandyBridge|SandyBridge-IBRS|",
+          "SapphireRapids|",
+          "Skylake-Client|Skylake-Client-IBRS|Skylake-Client-noTSX-IBRS|Skylake-Client-v4|",
+          "Skylake-Server|Skylake-Server-IBRS|Skylake-Server-noTSX-IBRS|Skylake-Server-v4|Skylake-Server-v5|",
+          "Westmere|Westmere-IBRS|",
+          "athlon|",
+          "core2duo|coreduo|",
+          "host|",
+          "kvm32|kvm64|",
+          "max|",
+          "pentium|pentium2|pentium3|",
+          "phenom|",
+          "qemu32|qemu64|",
+          "x86-64-v2|x86-64-v2-AES|x86-64-v3|x86-64-v4|",
+          "custom-[A-Za-z0-9@#$%^&*()-_=+]+",
+          ")$"
+        ]), 
+        flag
+      )
+    )
     error_message = join("", [
       "CPU type must be one of the following: ",
       "['486', ",
