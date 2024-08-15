61746f63dc4187cda2f6c162fc7690d943613b53
nodadyoushutup <admin@nodadyoushutup.com>
2024-08-15 02:48:45 +0000

    [WIP] Vault

--- vault/local.tf
+++ vault/local.tf
@@ -2,4 +2,5 @@
 
 locals {
   virtual_machine = yamldecode(file("/mnt/workspace/virtual_machine.yaml"))
+  provider = yamldecode(file("/mnt/workspace/provider.yaml"))
 }
--- vault/main.tf
+++ vault/main.tf
@@ -1,104 +1,23 @@
 # vault/main.tf
 
-module "vault" {
-  source  = "spacelift.io/nodadyoushutup/virtual_machine/proxmox"
-  # version = "0.4.5"
-
-  ###
-  # acpi = true
-  # agent = {
-  #   enabled = false
-  #   timeout = "15m"
-  #   trim = false
-  #   type = "virtio"
-  # }
-  # audio_device = {
-  #   device = "intel-hda"
-  #   driver = "spice"
-  #   enabled = false
-  # }
-  # bios = "seabios"
-  # boot_order = ["scsi0"]
-  # cdrom = {
-  #   enabled = false
-  #   file_id = "local:iso/TrueNAS-SCALE-24.04.0.iso"
-  #   interface = "ide3"
-  # }
-  # clone = {
-  #   datastore_id = "local-lvm"
-  #   node_name = "pve"
-  #   retries = 5
-  #   vm_id = 900
-  #   full = true
-  # }
-  # cpu = {
-  #   architecture = "x86_64"
-  #   cores = 2
-  #   flags = [
-  #     # "-aes",
-  #     # "-amd-no-ssb",
-  #     # "-amd-ssbd",
-  #     # "-hv-evmcs",
-  #     # "-hv-tlbflush",
-  #     # "-ibpb",
-  #     # "-md-clear",
-  #     # "-pcid",
-  #     # "-pdpe1gb",
-  #     # "-spec-ctrl",
-  #     # "-ssbd",
-  #     # "-virt-ssbd"
-  #   ]
-  #   hotplugged = 0
-  #   limit = 0
-  #   numa = false
-  #   sockets = 1
-  #   type = "host"
-  #   units = 1024
-  #   affinity = null
-  # }
-  # description = "Test virtual machine"
-  # disk = [
-  #   {
-  #     aio = "io_uring"
-  #     backup = true
-  #     cache = "none"
-  #     datastore_id = "local-lvm"
-  #     path_in_datastore = null
-  #     discard = "ignore"
-  #     file_format = "qcow2"
-  #     file_id = "local:iso/jammy-server-cloudimg-amd64.img"
-  #     interface = "scsi0"
-  #     iothread = false
-  #     replicate = true
-  #     serial = null
-  #     size = 8
-  #     speed = {
-  #       iops_read = null
-  #       iops_read_burstable = null
-  #       iops_write = null
-  #       iops_write_burstable = null
-  #       read = null
-  #       read_burstable = null
-  #       write = null
-  #       write_burstable = null
-  #     }
-  #     ssd = false
-  #   }
-  # ]
-  
-  ###
-  vm = {
-    id = "101"
-    name = "vault"
-    tags = ["terraform", "vault"]
-    node_name = "pve"
-    on_boot = false
-    started = false
-    stop_on_destroy = true
+resource "proxmox_virtual_environment_vm" "vault" {
+  name        = "vault"
+  description = "HashiCorp Vault"
+  tags        = ["terraform", "ubuntu"]
+  node_name = "pve"
+  vm_id     = 101
+  stop_on_destroy = true
+  agent {
+    enabled = false
   }
-  
-  initialization = {
-    ip_config = {
+  disk {
+    datastore_id = "local-lvm"
+    file_id      = "local:iso/jammy-server-cloudimg-amd64.img"
+    interface    = "scsi0"
+    size         = 10
+  }
+  initialization {
+    ip_config {
       ipv4 = {
         address = "192.168.1.101/24"
         gateway = "192.168.1.1"
@@ -107,12 +26,8 @@ module "vault" {
     user_account = {
       username = local.virtual_machine.username
       password = local.virtual_machine.password
-      keys = []
+      keys = [file("/mnt/workspace/work.pub")]
     }
-  }
-
-  memory = {
-    dedicated = 8192
   }
 }
 
