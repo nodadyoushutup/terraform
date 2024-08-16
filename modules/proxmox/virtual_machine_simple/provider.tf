1ef35e46e6cd40d34fe3060ee62304b2a2d77430
nodadyoushutup <admin@nodadyoushutup.com>
2024-08-16 21:43:19 +0000

    [WIP] Virtual Machine

--- modules/proxmox/virtual_machine_simple/provider.tf
+++ modules/proxmox/virtual_machine_simple/provider.tf
@@ -4,26 +4,26 @@ output "provider" {
   value = local.provider
 }
 
-# terraform {
-#   required_providers {
-#     proxmox = {
-#       source = "bpg/proxmox"
-#     }
-#   }
-# }
+terraform {
+  required_providers {
+    proxmox = {
+      source = "bpg/proxmox"
+    }
+  }
+}
 
-# provider "proxmox" {
-#   endpoint = local.provider.proxmox.endpoint
-#   api_token = local.provider.proxmox.api_token
-#   insecure  = local.provider.proxmox.insecure
-#   ssh {
-#     agent = true
-#     agent_socket = 22
-#     username = local.provider.proxmox.username
-#     private_key = file("/mnt/workspace/proxmox.pem")
-#     node {
-#       name = local.provider.proxmox.node.name
-#       address = local.provider.proxmox.node.address
-#     }
-#   }
-# }
+provider "proxmox" {
+  endpoint = local.provider.proxmox.endpoint
+  api_token = local.provider.proxmox.api_token
+  insecure  = local.provider.proxmox.insecure
+  ssh {
+    agent = true
+    agent_socket = 22
+    username = local.provider.proxmox.username
+    private_key = file("/mnt/workspace/proxmox.pem")
+    node {
+      name = local.provider.proxmox.node.name
+      address = local.provider.proxmox.node.address
+    }
+  }
+}
