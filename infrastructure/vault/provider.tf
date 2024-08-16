f173d60db9c86e17cc1b287070b1ce28fe129fb2
nodadyoushutup <admin@nodadyoushutup.com>
2024-08-16 21:24:03 +0000

    [WIP] Virtual Machine

--- infrastructure/vault/provider.tf
+++ infrastructure/vault/provider.tf
@@ -1,25 +1,25 @@
 # modules/proxmox/virtual_machine/provider.tf
 
-terraform {
-  required_providers {
-    proxmox = {
-      source = "bpg/proxmox"
-    }
-  }
-}
+# terraform {
+#   required_providers {
+#     proxmox = {
+#       source = "bpg/proxmox"
+#     }
+#   }
+# }
 
-provider "proxmox" {
-  endpoint = local.provider.proxmox.endpoint
-  api_token = local.provider.proxmox.api_token
-  insecure  = local.provider.proxmox.insecure
-  ssh {
-    agent = true
-    agent_socket = 22
-    username = local.provider.proxmox.username
-    private_key = file("/mnt/workspace/proxmox.pem")
-    node {
-      name = local.provider.proxmox.node.name
-      address = local.provider.proxmox.node.address
-    }
-  }
-}
+# provider "proxmox" {
+#   endpoint = local.provider.proxmox.endpoint
+#   api_token = local.provider.proxmox.api_token
+#   insecure  = local.provider.proxmox.insecure
+#   ssh {
+#     agent = true
+#     agent_socket = 22
+#     username = local.provider.proxmox.username
+#     private_key = file("/mnt/workspace/proxmox.pem")
+#     node {
+#       name = local.provider.proxmox.node.name
+#       address = local.provider.proxmox.node.address
+#     }
+#   }
+# }
--- modules/proxmox/virtual_machine_simple/local.tf
+++ modules/proxmox/virtual_machine_simple/local.tf
@@ -1,5 +1,6 @@
 # modules/proxmox/virtual_machine/local.tf
 
 locals {
+  virtual_machine = yamldecode(file("/mnt/workspace/virtual_machine.yaml"))
   provider = yamldecode(file("/mnt/workspace/provider.yaml"))
 }
