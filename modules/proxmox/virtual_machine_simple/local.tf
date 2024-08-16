0047ca704ab60dcb9ab548f9258f92daf532855c
nodadyoushutup <admin@nodadyoushutup.com>
2024-08-16 23:06:15 +0000

    [WIP] Virtual Machine

--- modules/proxmox/virtual_machine_simple/local.tf
+++ modules/proxmox/virtual_machine_simple/local.tf
@@ -2,4 +2,9 @@
 
 locals {
   virtual_machine = try(yamldecode(file("/mnt/workspace/virtual_machine.yaml")), {})
+  provider = try(yamldecode(file("/mnt/workspace/provider.yaml")), {})
+}
+
+output "provider" {
+  value = local.provider
 }
