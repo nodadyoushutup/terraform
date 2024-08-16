865015e0eecd7464a280fcca224cfbc67256af93
nodadyoushutup <admin@nodadyoushutup.com>
2024-08-16 23:25:38 +0000

    [WIP] Virtual Machine

--- infrastructure/vault/main.tf
+++ infrastructure/vault/main.tf
@@ -4,24 +4,24 @@ module "vault" {
   source  = "spacelift.io/nodadyoushutup/virtual_machine_simple/proxmox"
   # version = "1.0.0"
   vm_id = 101
-  name = "vault"
-  description = "HashiCorp Vault"
-  tags = ["terraform", "ubuntu", "vault", "test"]
-  disk = {
-    size = 20
-  }
-  memory = {
-    dedicated = 4096
-  }
-  cpu = {
-    cores = 1
-  }
-  initialization = {
-    ip_config = {
-      ipv4 = {
-        address = "192.168.1.101/24"
-        gateway = "192.168.1.1"
-      }
-    }
-  }
+  # name = "vault"
+  # description = "HashiCorp Vault"
+  # tags = ["terraform", "ubuntu", "vault", "test"]
+  # disk = {
+  #   size = 20
+  # }
+  # memory = {
+  #   dedicated = 4096
+  # }
+  # cpu = {
+  #   cores = 1
+  # }
+  # initialization = {
+  #   ip_config = {
+  #     ipv4 = {
+  #       address = "192.168.1.101/24"
+  #       gateway = "192.168.1.1"
+  #     }
+  #   }
+  # }
 }
