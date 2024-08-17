af0acc19703a49bb5923f2c7805996fe6b07ed03
nodadyoushutup <admin@nodadyoushutup.com>
2024-08-17 17:57:33 +0000

    [WIP] Fortigate

--- config/fortigate/main.tf
+++ config/fortigate/main.tf
@@ -70,7 +70,6 @@ module "port_forward_monitoring_ssh" {
   }  
 }
 
-
 module "port_forward_database_ssh" {
   source  = "spacelift.io/nodadyoushutup/port_forward/fortigate"
   # version = "1.0.0"
@@ -110,5 +109,131 @@ module "port_forward_database_mysql" {
   }
   dstaddr = {
     name = "database_mysql"
+  }  
+}
+
+module "port_forward_npm_ssh" {
+  source  = "spacelift.io/nodadyoushutup/port_forward/fortigate"
+  # version = "1.0.0"
+  mappedport = 22
+  extport = 10422
+  name = "npm_ssh"
+  mappedip = {
+    range = "192.168.1.104"
+  }
+  dstaddr = {
+    name = "npm_ssh"
+  }  
+}
+
+module "port_forward_development_ssh" {
+  source  = "spacelift.io/nodadyoushutup/port_forward/fortigate"
+  # version = "1.0.0"
+  mappedport = 22
+  extport = 10522
+  name = "development_ssh"
+  mappedip = {
+    range = "192.168.1.105"
+  }
+  dstaddr = {
+    name = "development_ssh"
+  }  
+}
+
+module "port_forward_k3s_master_1_ssh" {
+  source  = "spacelift.io/nodadyoushutup/port_forward/fortigate"
+  # version = "1.0.0"
+  mappedport = 22
+  extport = 20022
+  name = "k3s_master_1_ssh"
+  mappedip = {
+    range = "192.168.1.200"
+  }
+  dstaddr = {
+    name = "k3s_master_1_ssh"
+  }  
+}
+
+module "port_forward_k3s_master_2_ssh" {
+  source  = "spacelift.io/nodadyoushutup/port_forward/fortigate"
+  # version = "1.0.0"
+  mappedport = 22
+  extport = 20122
+  name = "k3s_master_2_ssh"
+  mappedip = {
+    range = "192.168.1.201"
+  }
+  dstaddr = {
+    name = "k3s_master_2_ssh"
+  }  
+}
+
+module "port_forward_k3s_master_3_ssh" {
+  source  = "spacelift.io/nodadyoushutup/port_forward/fortigate"
+  # version = "1.0.0"
+  mappedport = 22
+  extport = 20222
+  name = "k3s_master_3_ssh"
+  mappedip = {
+    range = "192.168.1.202"
+  }
+  dstaddr = {
+    name = "k3s_master_3_ssh"
+  }  
+}
+
+module "port_forward_k3s_worker_1_ssh" {
+  source  = "spacelift.io/nodadyoushutup/port_forward/fortigate"
+  # version = "1.0.0"
+  mappedport = 22
+  extport = 20322
+  name = "k3s_worker_1_ssh"
+  mappedip = {
+    range = "192.168.1.203"
+  }
+  dstaddr = {
+    name = "k3s_worker_1_ssh"
+  }  
+}
+
+module "port_forward_k3s_worker_2_ssh" {
+  source  = "spacelift.io/nodadyoushutup/port_forward/fortigate"
+  # version = "1.0.0"
+  mappedport = 22
+  extport = 20422
+  name = "k3s_worker_2_ssh"
+  mappedip = {
+    range = "192.168.1.204"
+  }
+  dstaddr = {
+    name = "k3s_worker_2_ssh"
+  }  
+}
+
+module "port_forward_k3s_worker_3_ssh" {
+  source  = "spacelift.io/nodadyoushutup/port_forward/fortigate"
+  # version = "1.0.0"
+  mappedport = 22
+  extport = 20522
+  name = "k3s_worker_3_ssh"
+  mappedip = {
+    range = "192.168.1.205"
+  }
+  dstaddr = {
+    name = "k3s_worker_3_ssh"
+  }  
+}
+
+module "port_forward_k3s_worker_4_ssh" {
+  source  = "spacelift.io/nodadyoushutup/port_forward/fortigate"
+  # version = "1.0.0"
+  mappedport = 22
+  extport = 20622
+  name = "k3s_worker_4_ssh"
+  mappedip = {
+    range = "192.168.1.206"
+  }
+  dstaddr = {
+    name = "k3s_worker_4_ssh"
   }  
 }
