dfd6e0deb3999bac48e4c558636307b758f500a5
nodadyoushutup <admin@nodadyoushutup.com>
2024-08-17 01:59:41 +0000

    [WIP] Database

--- infrastructure/spacelift/object/stack.tf
+++ infrastructure/spacelift/object/stack.tf
@@ -1,5 +1,17 @@
 # spacelift/stack.tf
 
+resource "spacelift_stack" "spacelift_attachment" {
+  administrative    = false
+  autodeploy        = true
+  branch            = "main"
+  description       = "Spacelift attachments"
+  name              = "spacelift_attachment"
+  project_root      = "/infrastructure/spacelift/attachment"
+  repository        = "terraform"
+  terraform_version = "1.5.7"
+  labels            = ["infrastructure", "spacelift_attachment"]
+}
+
 resource "spacelift_stack" "proxmox" {
   administrative    = false
   autodeploy        = true
