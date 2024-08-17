29663eb462ab26412921d49c3cc34f88aa020677
nodadyoushutup <admin@nodadyoushutup.com>
2024-08-17 14:33:35 +0000

    [WIP] Fortigate

--- config/fortigate/main.tf
+++ config/fortigate/main.tf
@@ -61,18 +61,18 @@ resource "fortios_firewall_vip" "trname" {
 }
 
 resource "fortios_firewall_policy" "trname" {
-  action             = "accept"
-  logtraffic         = "utm"
-  name               = "policys1"
-  policyid           = 1
-  schedule           = "always"
-  wanopt             = "disable"
-  wanopt_detection   = "active"
-  wanopt_passive_opt = "default"
-  wccp               = "disable"
-  webcache           = "disable"
-  webcache_https     = "disable"
-  wsso               = "enable"
+#   action             = "accept"
+#   logtraffic         = "utm"
+#   name               = "policys1"
+#   policyid           = 1
+#   schedule           = "always"
+#   wanopt             = "disable"
+#   wanopt_detection   = "active"
+#   wanopt_passive_opt = "default"
+#   wccp               = "disable"
+#   webcache           = "disable"
+#   webcache_https     = "disable"
+#   wsso               = "enable"
 
 #   dstaddr {
 #     name = "all"
