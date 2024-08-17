2b1ebafcc66d5e0196b2b56bc0b0ff888d3e69a6
nodadyoushutup <admin@nodadyoushutup.com>
2024-08-17 13:38:36 +0000

    [WIP] Fortigate

---
+++ config/fortigate/main.tf
@@ -0,0 +1,13 @@
+# config/firtugate/main.tf
+
+resource "fortios_firewall_address" "trname" {
+  allow_routing        = "disable"
+  associated_interface = "port2"
+  color                = 3
+  end_ip               = "255.255.255.0"
+  name                 = "testaddress"
+  start_ip             = "22.1.1.0"
+  subnet               = "22.1.1.0 255.255.255.0"
+  type                 = "ipmask"
+  visibility           = "enable"
+}
