0458382ced723cbb3f02227fa98c52bb38f6d162
nodadyoushutup <admin@nodadyoushutup.com>
2024-08-17 16:04:25 +0000

    [WIP] Policy

--- modules/fortigate/policy/main.tf
+++ modules/fortigate/policy/main.tf
@@ -1,29 +1,29 @@
 resource "fortios_firewall_policy" "policy_truenas" {
-  action             = "accept"
-  logtraffic         = "all"
-  name               = "truenas"
-  policyid           = 100
-  schedule           = "always"
-  wanopt             = "disable"
-  wanopt_detection   = "off"
-  wanopt_passive_opt = "default"
-  wccp               = "disable"
-  webcache           = "disable"
-  webcache_https     = "disable"
-  wsso               = "disable"
-  dstaddr {
-    name = "truenas"
-  }
-  dstintf {
-    name = "lan"
-  }
-  service {
-    name = "ALL"
-  }
-  srcaddr {
-    name = "all"
-  }
-  srcintf {
-    name = "wan"
-  }
+    action             = var.action
+    logtraffic         = var.logtraffic
+    name               = var.name
+    policyid           = var.policyid
+    schedule           = var.schedule
+    wanopt             = var.wanopt
+    wanopt_detection   = var.wanopt_detection
+    wanopt_passive_opt = var.wanopt_passive_opt
+    wccp               = var.wccp
+    webcache           = var.webcache
+    webcache_https     = var.webcache_https
+    wsso               = var.wsso
+    dstaddr {
+        name = var.dstaddr_name
+    }
+    dstintf {
+        name = var.dstintf_name
+    }
+    service {
+        name = var.service_name
+    }
+    srcaddr {
+        name = var.srcaddr_name
+    }
+    srcintf {
+        name = var.srcintf_name
+    }
 }
