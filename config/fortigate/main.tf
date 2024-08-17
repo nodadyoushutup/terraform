310a5ed875252af3c46125115a650348518be4ac
nodadyoushutup <admin@nodadyoushutup.com>
2024-08-17 16:50:31 +0000

    [WIP] Fortigate

--- config/fortigate/main.tf
+++ config/fortigate/main.tf
@@ -1,25 +1,5 @@
 # config/firtugate/main.tf
 
-# module "vip_truenas" {
-#   source  = "spacelift.io/nodadyoushutup/vip/fortigate"
-#   # version = "1.0.0"
-#   mappedport = 80
-#   fosid = 100
-#   name = "truenas"
-#   extport = 100
-#   mappedip = {
-#     range = "192.168.1.100"
-#   } 
-# }
-
-# module "firewall_policy_truenas" {
-#   source  = "spacelift.io/nodadyoushutup/policy/fortigate"
-#   # version = "1.0.0"
-#   name               = "truenas"
-#   policyid           = 100
-#   dstaddr_name       = "truenas"
-# }
-
 module "port_forward_truenas" {
   source  = "spacelift.io/nodadyoushutup/port_forward/fortigate"
   # version = "1.0.0"
@@ -31,5 +11,19 @@ module "port_forward_truenas" {
   }
   dstaddr = {
     name = "truenas"
+  }  
+}
+
+module "port_forward_vault" {
+  source  = "spacelift.io/nodadyoushutup/port_forward/fortigate"
+  # version = "1.0.0"
+  mappedport = 80
+  extport = 101
+  name = "vault"
+  mappedip = {
+    range = "192.168.1.101"
+  }
+  dstaddr = {
+    name = "vault"
   }  
 }
