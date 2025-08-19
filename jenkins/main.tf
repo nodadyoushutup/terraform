7e507eb28c1a7bce1458ef2a69f5d40869d40f9e
nodadyoushutup <admin@nodadyoushutup.com>
2025-08-19 00:16:26 +0000

    terraform pipeline

--- jenkins/main.tf
+++ jenkins/main.tf
@@ -1,6 +1,13 @@
 resource "jenkins_job" "cloud_image" {
   name     = "cloud-image"
-  template = templatefile("${path.module}/job.xml", {
-    description = "An cloud_image job created from Terraform"
+  template = templatefile("${path.module}/job/cloud_image.xml", {
+    description = "A cloud_image job created with Packer"
+  })
+}
+
+resource "jenkins_job" "proxmox" {
+  name     = "proxmox"
+  template = templatefile("${path.module}/job/terraform.xml", {
+    description = "Proxmox assets"
   })
 }
