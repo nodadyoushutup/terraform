f73ce57dde467e9c0010b28dc4a56c339b008628
nodadyoushutup <admin@nodadyoushutup.com>
2025-08-19 00:42:28 +0000

    terraform

--- main.tf
+++ main.tf
@@ -1,16 +1,19 @@
 resource "jenkins_job" "cloud_image" {
-  name     = "cloud-image"
-  template = templatefile("${path.module}/job/cloud_image.xml", {
+  name = "cloud-image"
+  template = templatefile("${path.module}/job.xml", {
     description = "A cloud_image job created with Packer"
+    project_url = "https://github.com/nodadyoushutup/cloud-image"
+    scm_repository_url = "https://github.com/nodadyoushutup/cloud-image"
+    script_path = "pipeline.jenkins"
   })
 }
 
-resource "jenkins_job" "proxmox" {
-  name     = "proxmox"
-  template = templatefile("${path.module}/job/terraform.xml", {
+resource "jenkins_job" "terraform_proxmox" {
+  name = "terraform-proxmox"
+  template = templatefile("${path.module}/job.xml", {
     description = "Proxmox assets"
-    project_url = "https://github.com/nodadyoushutup/terraform"
-    scm_repository_url = "https://github.com/nodadyoushutup/terraform"
-    script_path = "terraform.jenkins"
+    project_url = "https://github.com/nodadyoushutup/terraform-proxmox"
+    scm_repository_url = "https://github.com/nodadyoushutup/terraform-proxmox"
+    script_path = "pipeline.jenkins"
   })
 }
